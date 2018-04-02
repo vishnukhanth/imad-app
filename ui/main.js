0414c15f07700603ade3bd53621d0aad58745dc1
vishnukhanth <36645605+vishnukhanth@users.noreply.github.com>
2018-04-02 03:09:43 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 alert('hi i am java script');
 var img = document.getElementById('madi');
 img.onclick = function(){
-  img.style.marginleft = "200px";
+  img.style.marginLeft = "200px";
   alert('Image clicked');
 };
